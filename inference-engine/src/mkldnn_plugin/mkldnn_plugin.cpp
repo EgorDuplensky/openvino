99c3b467d52084d12a8bc4c96f24ca67ee69e7e7
dmitrygo <dmitry.gorokhov@intel.com>
2021-08-31 16:19:42 +0300

    [CPU] Enabled I8 precision on activations for Convolution node

--- inference-engine/src/mkldnn_plugin/mkldnn_plugin.cpp
+++ inference-engine/src/mkldnn_plugin/mkldnn_plugin.cpp
@@ -343,7 +343,7 @@ static void Transformation(CNNNetwork& clonedNetwork, const Config& conf) {
 
         auto supportedPrecisions = std::vector<OperationPrecisionRestriction>({
             OperationPrecisionRestriction::create<ngraph::opset1::Convolution>({
-                {0, {ngraph::element::u8}},
+                {0, {ngraph::element::u8, ngraph::element::i8}},
                 {1, {ngraph::element::i8}},
             }),
             OperationPrecisionRestriction::create<ngraph::opset1::ConvolutionBackpropData>({
